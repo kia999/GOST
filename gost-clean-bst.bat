512cab30a1ecfcd837cf311317876dedabeff535
LSinev <LSinev@users.noreply.github.com>
2017-01-12 01:51:54 +0300

    Fix mistake in cleaning script

--- gost-clean-bst.bat
+++ gost-clean-bst.bat
@@ -7,8 +7,8 @@
 @cd /d "%~dp0doc\bibtex\gost\"
 @echo Удаление файлов из подпапки doc\bibtex\gost\:
 @del /S *.bst
-@cd /d "%~dp0bibtex\gost\"
-@echo Удаление файлов из подпапки bibtex\gost\:
+@cd /d "%~dp0bibtex\bst\gost\"
+@echo Удаление файлов из подпапки bibtex\bst\gost\:
 @del /S *.bst
 @cd /d "%~dp0tex\latex\gost\"
 @echo Удаление файлов из подпапки tex\latex\gost\:
