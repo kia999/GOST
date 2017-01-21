59a628b3d14006a0375b065828312df0f3ac20d2
LSinev <LSinev@users.noreply.github.com>
2017-01-21 16:04:15 +0300

    Remove gost.tds.zip from putting to CTAN
    Remove gost.tds.zip from putting into gost.zip (archive for CTAN).

--- gost-zip-ctan.bat
+++ gost-zip-ctan.bat
@@ -8,9 +8,7 @@
 @call "%~dp0gost-prepare-files.cmd"
 @echo Вызов скрипта подготовки файлов для CTAN в папке gost\
 @call "%~dp0gost-create-gost-folder-after-prepare.cmd"
-@echo Вызов скрипта подготовки файлов в архиве со структурой TDS
-@call "%~dp0gost-zip-tds-after-prepare.bat"
 @echo Непосредственно архивирование папки gost\
-zip -r gost.zip gost gost.tds.zip -x *.bst *.blg *.bbl *.bak 
+zip -r gost.zip gost -x *.bst *.blg *.bbl *.bak
 @echo Вызов скрипта удаления папки gost\
 @call "%~dp0gost-remove-gost-folder.bat"
